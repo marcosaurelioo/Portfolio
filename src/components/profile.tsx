415380a4f4622120946a7d0f9f9f3d02cd9f9458
Maark007 <marcosaureliolelo@outlook.com>
2023-01-24 00:23:52 -0300

    feat: adding portfolio page

---
+++ src/components/profile.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { Hubballi } from "@next/font/google";
+
+import LinkedinIcon from "@/assets/linkedin.svg";
+import GithubIcon from "@/assets/github.svg";
+import ResumeIcon from "@/assets/resume.svg";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const hubballiTypography = Hubballi({ weight: "400" });
+
+export function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-header-line" />
+
+      <div className="profile-title">
+        <h1 className={hubballiTypography.className}>Marcos Aurélio</h1>
+        <h3 className={hubballiTypography.className}>Software Developer</h3>
+      </div>
+
+      <article className="profile-description">
+        <p className={hubballiTypography.className}>
+          A Software Enthusiast, I am a Brazilian self-taught Software Engineer
+          who likes to create operational and attractive applications. Involved
+          with computers since I was a child, I wrote my first algorithm when I
+          was 16 years old, and since then I have been working on creating
+          solutions with code. The motivation behind the code I write is to
+          outsource responsibilities to machines, I believe this process brings
+          convenience to people.
+        </p>
+
+        <p className={hubballiTypography.className}>
+          As an experienced full-stack developer, I have been working with
+          Reactjs, Nodejs, Nextjs, unit testing, test coverage, JavaScript, and
+          TypeScript. I am always willing to learn new technologies like
+          artificial intelligence. AI has been a subject of great interest to
+          me, the idea of creating a program capable of autonomously solving
+          problems is almost magical.
+        </p>
+
+        <p className={hubballiTypography.className}>
+          I constantly seek to add value to my work, learn new tools, stay up on
+          technology news and participate in developer communities.
+        </p>
+
+        <p className={hubballiTypography.className}>Get in touch.</p>
+      </article>
+
+      <div className="profile-contact">
+        <span className={hubballiTypography.className}>
+          marcosaureliolelo@outlook.com
+        </span>
+
+        <div className="profile-contact-icons-box">
+          <Link href="/resume.pdf" download target="_blank">
+            <Image src={ResumeIcon} alt="resume-icon" />
+          </Link>
+
+          <Link href="https://github.com/marcosaurelioo" target="_blank">
+            <Image src={GithubIcon} alt="github-icon" />
+          </Link>
+
+          <Link
+            href="https://linkedin.com/in/marcos-aurelio-goncalves"
+            target="_blank"
+          >
+            <Image src={LinkedinIcon} alt="linkedin-icon" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
